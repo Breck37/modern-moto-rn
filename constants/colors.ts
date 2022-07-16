dd9d9db608e9551c741755fdfcd4e8bbccf9f1c0
Brent Eckert <brent.eckert7@gmail.com>
2022-07-15 22:23:02 -0700

    chore: initialized color palettes

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,2 +1,19 @@
 export const PRIMARY='#820DDC'
 export const BLUE='#2472FC'
+
+
+export const BRIGHT_PALETTE = {
+    primary:  '#00aeef',  
+    secondary: '#ed008c',  
+    accent: '#fff000',  
+    tertiary: '#000000',
+    extra: '#8ac926'
+}
+
+export const PURPLE_PALETTE = {
+    primary:  '#8f53fe',  
+    secondary: '#7a71f8',  
+    accent: '#3bcae5',  
+    tertiary: '#658ef2',
+    extra: '50aceb'
+}
